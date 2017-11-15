d7624e485aade201ab3fb9a7d2780887b7411e0f
joao reis <jpagsreis@gmail.com>
2017-11-15 09:09:49 +0000

    alteraçao teste

--- src/gyroscopeDriver.c
+++ src/gyroscopeDriver.c
@@ -4,16 +4,46 @@
 
 void gyroInit(int8_t cs, int8_t miso, int8_t mosi, int8_t clk) 
 {
-	// GPIO structure declaration
-	GPIO_InitTypeDef GPIO_InitStruct;
-	// Enabling GPIO peripheral clock
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
-	// GPIO peripheral properties specification
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_5; // GPIO pin
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT; // output
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz; // clock speed
-	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP; // push/pull 
-	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL; // pullup/pulldown resistors inactive
+	GPIO_InitTypeDef *GPIO_InitStruct;
+	
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE); //enable SCK, MOSI, MISO and NSS GPIO clocks
+	
+	/*(#) Peripherals alternate function: 
+       (++) Connect the pin to the desired peripherals' Alternate Function (AF) 
+            using GPIO_PinAFConfig() function
+       (++) Configure the desired pin in alternate function by: 
+            GPIO_InitStruct->GPIO_Mode = GPIO_Mode_AF
+       (++) Select the type, pull-up/pull-down and output speed via GPIO_PuPd, 
+            GPIO_OType and GPIO_Speed members
+       (++) Call GPIO_Init() function In I2S mode, if an external clock source is 
+            used then the I2S CKIN pin should be also configured in Alternate 
+            function Push-pull pull-up mode. */
+	
+	
+	//can use GPIO_StructInit() to fill each GPIO_InitStruct member with its default value
+	GPIO_InitStruct->GPIO_Mode = GPIO_Mode_AF; // alternate function
+	GPIO_InitStruct->GPIO_Speed = GPIO_Speed_50MHz; // clock speed
+	GPIO_InitStruct->GPIO_OType = GPIO_OType_PP; // push/pull 
+	GPIO_InitStruct->GPIO_PuPd = GPIO_PuPd_NOPULL; // pullup/pulldown resistors inactive
+		
+	/* SPI SCK pin configuration */	
+	GPIO_InitStruct->GPIO_Pin = GPIO_Pin_5; // GPIO pin		
 	// Setting GPIO peripheral corresponding bits
-	GPIO_Init(GPIOA, &GPIO_InitStruct);
+	GPIO_Init(GPIOA, GPIO_InitStruct);		
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource5, GPIO_AF_SPI1); 
+	
+	/* SPI MOSI pin configuration */
+  GPIO_InitStruct->GPIO_Pin =  GPIO_Pin_7; //isto sao masks logo n se substituem
+  GPIO_Init(GPIOA, GPIO_InitStruct);
+  GPIO_PinAFConfig(GPIOA, GPIO_PinSource7, GPIO_AF_SPI1);
+	
+	/* SPI MISO pin configuration */
+  GPIO_InitStruct->GPIO_Pin =  GPIO_Pin_6; //isto sao masks logo n se substituem
+  GPIO_Init(GPIOA, GPIO_InitStruct);
+  GPIO_PinAFConfig(GPIOA, GPIO_PinSource6, GPIO_AF_SPI1);
+	
+	/* SPI NSS pin configuration */
+  GPIO_InitStruct->GPIO_Pin =  GPIO_Pin_15; //isto sao masks logo n se substituem
+  GPIO_Init(GPIOA, GPIO_InitStruct);
+  GPIO_PinAFConfig(GPIOA, GPIO_PinSource15, GPIO_AF_SPI1);
 }
--- src/spiModule.c
+++ src/spiModule.c
@@ -7,55 +7,13 @@
 void spiInit()
 {
 	SPI_InitTypeDef *SPI_InitStruct;
-	GPIO_InitTypeDef *GPIO_InitStruct;
-	
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE); //enable SCK, MOSI, MISO and NSS GPIO clocks
-	
-	/*(#) Peripherals alternate function: 
-       (++) Connect the pin to the desired peripherals' Alternate Function (AF) 
-            using GPIO_PinAFConfig() function
-       (++) Configure the desired pin in alternate function by: 
-            GPIO_InitStruct->GPIO_Mode = GPIO_Mode_AF
-       (++) Select the type, pull-up/pull-down and output speed via GPIO_PuPd, 
-            GPIO_OType and GPIO_Speed members
-       (++) Call GPIO_Init() function In I2S mode, if an external clock source is 
-            used then the I2S CKIN pin should be also configured in Alternate 
-            function Push-pull pull-up mode. */
-	
-	
-	//can use GPIO_StructInit() to fill each GPIO_InitStruct member with its default value
-	GPIO_InitStruct->GPIO_Mode = GPIO_Mode_AF; // alternate function
-	GPIO_InitStruct->GPIO_Speed = GPIO_Speed_50MHz; // clock speed
-	GPIO_InitStruct->GPIO_OType = GPIO_OType_PP; // push/pull 
-	GPIO_InitStruct->GPIO_PuPd = GPIO_PuPd_NOPULL; // pullup/pulldown resistors inactive
-		
-	/* SPI SCK pin configuration */	
-	GPIO_InitStruct->GPIO_Pin = GPIO_Pin_5; // GPIO pin		
-	// Setting GPIO peripheral corresponding bits
-	GPIO_Init(GPIOA, GPIO_InitStruct);		
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource5, GPIO_AF_SPI1); 
-	
-	/* SPI MOSI pin configuration */
-  GPIO_InitStruct->GPIO_Pin =  GPIO_Pin_7; //isto sao masks logo n se substituem
-  GPIO_Init(GPIOA, GPIO_InitStruct);
-  GPIO_PinAFConfig(GPIOA, GPIO_PinSource7, GPIO_AF_SPI1);
-	
-	/* SPI MISO pin configuration */
-  GPIO_InitStruct->GPIO_Pin =  GPIO_Pin_6; //isto sao masks logo n se substituem
-  GPIO_Init(GPIOA, GPIO_InitStruct);
-  GPIO_PinAFConfig(GPIOA, GPIO_PinSource6, GPIO_AF_SPI1);
-	
-	/* SPI NSS pin configuration */
-  GPIO_InitStruct->GPIO_Pin =  GPIO_Pin_15; //isto sao masks logo n se substituem
-  GPIO_Init(GPIOA, GPIO_InitStruct);
-  GPIO_PinAFConfig(GPIOA, GPIO_PinSource15, GPIO_AF_SPI1);
-	
+
 	/**
   * @brief  Fills each SPI_InitStruct member with its default value.
   * @param  SPI_InitStruct: pointer to a SPI_InitTypeDef structure which will be initialized.
   * @retval None
   */
-	SPI_StructInit(SPI_InitStruct);
+	SPI_StructInit(SPI_InitStruct); //SPI_DataSize member=8bits
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE); //enable peripheral clock	
 	
 	/**
@@ -75,14 +33,11 @@ void spiInit()
   * @retval None
   */
 	SPI_Cmd(SPI1, ENABLE);
-	/**
-  * @brief  Configures the data size for the selected SPI.
-  * @param  SPIx: where x can be 1, 2, 3, 4, 5 or 6 to select the SPI peripheral.
-  * @param  SPI_DataSize: specifies the SPI data size.
-  *          This parameter can be one of the following values:
-  *            @arg SPI_DataSize_16b: Set data frame format to 16bit
-  *            @arg SPI_DataSize_8b: Set data frame format to 8bit
-  * @retval None
-  */
-	SPI_DataSizeConfig(SPI1, SPI_DataSize_8b); 
 }
+
+void spiWrite(uint8_t data)
+{
+	
+}
+
+uint8_t spiRead
